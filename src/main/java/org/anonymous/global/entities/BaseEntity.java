db3b6fd4a61ded645b8b9299ab3c91b63d85193c
JINJU PARK <aricclunj@naver.com>
2025-02-02 12:44:34 +0900

    주석, 멤버쪽 토큰 받아올때 필요한 코드 추가

--- src/main/java/org/anonymous/global/entities/BaseEntity.java
+++ src/main/java/org/anonymous/global/entities/BaseEntity.java
@@ -1,5 +1,6 @@
 package org.anonymous.global.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -10,6 +11,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+/**
+ * 모든 Entity 의 공통 속성화
+ *
+ * 추상 Class
+ */
+// 다른 Entity의 상위 Class임을 알려주는 Annotation, Table 순서는 변경 불가
+// 해당 어노테이션 오류 날 시 build.gradle에서 jakarta.persistence-api의 버전을 설정 후 인텔리제이 재구동
 @Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -17,11 +25,14 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
     private LocalDateTime createdAt; // 등록일시
 
     @LastModifiedDate
     @Column(insertable = false)
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
     private LocalDateTime modifiedAt; // 수정일시
 
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
     private LocalDateTime deletedAt; // 삭제일시
 }
